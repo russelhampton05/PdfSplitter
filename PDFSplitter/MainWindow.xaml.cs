cfa4f28ee1fbcca3b26fe18633c86c42a76d632b
Russel Hampton <russel.hampton@gmail.com>
2018-12-25 20:42:16 -0600

    init

---
+++ PDFSplitter/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace PDFSplitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        string filePath { get; set; }
+        string destinationDirectory { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
+        }
+     
+        private void SelectFileClick(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if(dialog.ShowDialog().Value)
+            {
+                filePath = dialog.FileName;
+            }
+        }
+
+
+        private void SelectDirectoryClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Directory clicked");
+        }
+
+
+        private void CreateClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+    }
+}
