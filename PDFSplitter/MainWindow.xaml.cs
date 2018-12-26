5b6e59167c9f2394d8a966eb1459d67e4f6dca02
Russel Hampton <russel.hampton@gmail.com>
2018-12-25 21:42:55 -0600

    added gitignore

--- PDFSplitter/MainWindow.xaml.cs
+++ PDFSplitter/MainWindow.xaml.cs
@@ -1,21 +1,13 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
+using System.Windows.Forms;
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.IO;
+using System.Linq;
 
 namespace PDFSplitter
 {
@@ -26,8 +18,36 @@ namespace PDFSplitter
 
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        string filePath { get; set; }
-        string destinationDirectory { get; set; }
+        private string _filePath;
+        private string _destinationDirectory;
+        private string _pageCounts;
+        public string filePath
+        {
+            get => _filePath;
+            set
+            {
+                _filePath = value;
+                OnPropertyChanged();
+            }
+        }
+        public string pageCounts
+        {
+            get => _pageCounts;
+            set
+            {
+                _pageCounts = value;
+                OnPropertyChanged();
+            }
+        }
+        public string destinationDirectory
+        {
+            get => _destinationDirectory;
+            set
+            {
+                _destinationDirectory = value;
+                OnPropertyChanged();
+            }
+        }
 
         public MainWindow()
         {
@@ -39,13 +59,13 @@ namespace PDFSplitter
         private void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             var handler = PropertyChanged;
-            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
+            handler?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-     
+
         private void SelectFileClick(object sender, RoutedEventArgs e)
         {
-            var dialog = new OpenFileDialog();
-            if(dialog.ShowDialog().Value)
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 filePath = dialog.FileName;
             }
@@ -54,13 +74,53 @@ namespace PDFSplitter
 
         private void SelectDirectoryClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Directory clicked");
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                destinationDirectory = dialog.SelectedPath;
+            }
         }
 
 
         private void CreateClick(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("test");
+            if(string.IsNullOrEmpty(filePath) || string.IsNullOrEmpty(destinationDirectory))
+            {
+                return;
+            }
+
+            // Open the file
+            PdfDocument inputDocument = PdfReader.Open(filePath, PdfDocumentOpenMode.Import);
+            var pages = pageCounts.Split(',').ToList();
+            var fileCount = 0;
+            var idx = 0;
+           while(idx < inputDocument.PageCount)
+            {
+                // Create new document
+                PdfDocument outputDocument = new PdfDocument();
+                outputDocument.Version = inputDocument.Version;
+                outputDocument.Info.Creator = inputDocument.Info.Creator;
+
+                // Add the page and save it
+                if(pages.Count > fileCount)
+                {
+                    var pageCount = int.Parse(pages[fileCount]);
+                    for (int x = 0; x < pageCount; x++)
+                    {
+                        outputDocument.AddPage(inputDocument.Pages[idx]);
+                        idx++;
+                    }
+                }
+                else
+                {
+                    outputDocument.AddPage(inputDocument.Pages[idx]);
+                    idx++;
+                }
+
+                outputDocument.Save(Path.Combine(destinationDirectory, $"{idx}_{Path.GetFileName(filePath)}"));
+                fileCount++;
+            }
         }
     }
 }
